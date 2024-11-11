67e7180a25bc8f51216fb5edf76393e2f556c076
KRKolev23 <KRKolev23@codingburgas.bg>
2024-11-11 23:29:34 +0200

    Add rulse and start game

--- division-x/src/main.cpp
+++ division-x/src/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
@@ -13,9 +13,18 @@ int main()
     {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BEIGE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("2048", 70, 100, 200, WHITE);
+        DrawText("Rulse:The game concept is simple and complex at the same time. ", 5, 340, 20, WHITE);
+        DrawText("To only rule of the game is that you have to", 5, 360, 20, WHITE);
+        DrawText("merge 2 tiles with the same numerical value and then they become 1 tile ", 5, 380, 20, WHITE);
+        DrawText(" with x2 of their original value then you have to merge this new x2 tile", 5, 400, 20, WHITE);
+        DrawText(" with another x2 tile until you reach 2048.", 5, 420, 20, WHITE);
+        DrawRectangleLines(  740,  125,  530,  450, WHITE );
+       
+
+        
 
         EndDrawing();
     }
